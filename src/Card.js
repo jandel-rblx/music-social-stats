128212b43359ecfb1b232bff5c0cbf7f74895703
Hunter <60201971+HooferDevelops@users.noreply.github.com>
2023-05-29 18:01:18 -0400

    fix spotify url being incorrect

--- src/Card.js
+++ src/Card.js
@@ -14,7 +14,7 @@ class Card extends React.Component {
                 <div className="container">
                     <img className="thumbnail" src={this.props.info.spotifyThumbnail || this.props.info.youtubeThumbnail} alt="Song Icon"></img>
                     <b className="name info">{this.props.info.name}</b>
-                    <a href={`https://www.spotify.com/track/${this.props.info.spotifyId}`} className={`views info ${this.props.info.spotifyId ? "active" : "inactive"}`}><i className="fa fa-spotify"/> <b>{<RollingNumber goal={this.props.info.spotifyStreams}/>}</b> Streams</a>
+                    <a href={`https://open.spotify.com/track/${this.props.info.spotifyId}`} className={`views info ${this.props.info.spotifyId ? "active" : "inactive"}`}><i className="fa fa-spotify"/> <b>{<RollingNumber goal={this.props.info.spotifyStreams}/>}</b> Streams</a>
                     <a href={`https://www.youtube.com/watch?v=${this.props.info.youtubeId}`} className={`views info ${this.props.info.youtubeId ? "active" : "inactive"}`}><i className="fa fa-youtube"/> <b>{<RollingNumber goal={this.props.info.youtubeStreams}/>}</b> Streams</a>
                 </div>
             </div>
